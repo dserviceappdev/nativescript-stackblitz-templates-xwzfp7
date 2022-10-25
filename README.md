8cfe053aee4c677061e1fd43ba8112afc7888ddd
dserviceappdev <116671831+dserviceappdev@users.noreply.github.com>
2022-10-25 19:59:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-xwzfp7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-xwzfp7)
---
+++ app/main-page.js
@@ -0,0 +1,75 @@
+// >> setting-url-webview
+import { Dialogs, Observable, WebView, Page } from '@nativescript/core';
+
+export function onNavigatingTo(args) {
+  const page = args.object;
+  const vm = new Observable();
+  vm.set('webViewSrc', 'https://6libera.d-serviceitalia.it');
+  vm.set('result', '');
+  vm.set('tftext', 'https://6libera.d-serviceitalia.it/');
+  page.bindingContext = vm;
+}
+// handling WebView load finish event
+export function onWebViewLoaded(webargs) {
+  const page = webargs.object.page;
+  const vm = page.bindingContext;
+  const webview = webargs.object;
+  page.actionBarHidden = true;
+  webview.android.getSettings().setBuiltInZoomControls(false);
+
+  vm.set('result', 'WebView is still loading...');
+  vm.set('enabled', false);
+
+  webview.on(WebView.loadFinishedEvent, (args) => {
+    let message = '';
+    if (!args.error) {
+      message = `WebView finished loading of ${args.url}`;
+    } else {
+      message = `Error loading ${args.url} : ${args.error}`;
+    }
+
+    vm.set('result', message);
+    console.log(`WebView message - ${message}`);
+  });
+}
+// going to the previous page if such is available
+export function goBack(args) {
+  const page = args.object.page;
+  const vm = page.bindingContext;
+  const webview = page.getViewById('myWebView');
+  if (webview.canGoBack) {
+    webview.goBack();
+    vm.set('enabled', true);
+  }
+}
+// going forward if a page is available
+export function goForward(args) {
+  const page = args.object.page;
+  const vm = page.bindingContext;
+  const webview = page.getViewById('myWebView');
+  if (webview.canGoForward) {
+    webview.goForward();
+  } else {
+    vm.set('enabled', false);
+  }
+}
+// changing WebView source
+export function submit(args) {
+  const page = args.object.page;
+  const vm = page.bindingContext;
+  const textField = args.object;
+  const text = textField.text;
+  vm.set('enabled', false);
+  if (text.substring(0, 4) === 'http') {
+    vm.set('webViewSrc', text);
+    textField.dismissSoftInput();
+  } else {
+    Dialogs.alert(
+      'Please, add `http://` or `https://` in front of the URL string'
+    ).then(() => {
+      console.log('Dialog closed!');
+    });
+  }
+}
+
+// << setting-url-webview
